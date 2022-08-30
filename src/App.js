27122c37c8a135cc8edcc6ca407fac994e2af7a4
Navi-Beep <estawars30@gmail.com>
2022-08-29 20:46:21 -0500

    trying to add birb

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Navbar from "./components/Navbar";
+//import cockatiel from "cockatiel.png";
 
 
 
@@ -17,17 +18,25 @@ function App(props) {
 
     return (
         <>
-        <Navbar name='Elise' city='Cat Town, USA'/>
-            <div className='container'>
+        <Navbar name='Elise'/>
+            <div className='container justify-content-center'>
+              
                 <h2 className='text-center'>Please enter what you need to do for today!</h2>
+                <div class='row bg-primary rounded-3'>
                 <form onSubmit={handleCatClick}>
-                    <input type='text' className='form-control' name='firstList' />
-                    <input type='submit' value='Submit' />
+                    <input type='text' className='d-flex form-control w-100 mt-3' name='firstList' />
+                    <input type='submit'
+                    className='text-center mt-3'
+                    value='Add to List' />
                 </form>
                 {lists.map((l, idx) => <p key={idx}>{l}</p>)}
+                </div>
             </div>
         </>
     )
 }
 
 export default App;
+
+
+//<a href="https://www.flaticon.com/free-icons/cockatiel" title="cockatiel icons">Cockatiel icons created by Freepik - Flaticon</a>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
+//import cockatiel from './cockatiel.png'
 
 
 export default function Navbar(props){
     return(
-        <nav class="navbar navbar-expand-lg bg-light">
+        <nav class="navbar navbar-expand-lg bg-dark">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Welcome to my to do list!</a>
+                    <a class="navbar-brand text-light" href="#">Welcome!</a>
+            
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
